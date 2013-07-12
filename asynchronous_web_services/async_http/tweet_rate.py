92bfed3dee2a76d4996dff277541961d41115ab3
zfz <zfz@zfzs-MacBook-Pro.local>
2013-07-12 18:44:04 +0800

    Update twitter api 1.1

--- asynchronous_web_services/async_http/tweet_rate.py
+++ asynchronous_web_services/async_http/tweet_rate.py
@@ -8,26 +8,47 @@ import urllib
 import json
 import datetime
 import time
+import oauth2 as oauth
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
+consumer_key = "bZzeXGYz1KpJFtDOhL6iA"
+consumer_secret = "lXtNjEYvFFy0p0rAZ39g5qRzXAVzmHHu6DXi6jxCM8"
+access_token = "43530233-AMI6KwoJxrjhpjYhzGSeBGTDNDL31glyS7lKYKK5h"
+access_secret = "an3tGgmAlM3J3X7vz3YdFYu87cKwHtTfUKTqqIoY5A"
+
+
+def request_twitter(url, key, secret, http_method = 'GET', post_body = '', http_headers = ''):
+    consumer = oauth.Consumer(key = consumer_key, secret = consumer_secret)
+    token = oauth.Token(key = key, secret = secret)
+    client = oauth.Client(consumer, token)
+
+    request = client.request(
+        url,
+        method = http_method,
+        body = post_body,
+        headers = http_headers)
+
+    return request
+
 class IndexHandler(tornado.web.RequestHandler):
-	def get(self):
-		query = self.get_argument('q')
-		client = tornado.httpclient.HTTPClient()
-		response = client.fetch("http://search.twitter.com/search.json?" + \
-				urllib.urlencode({"q": query, "result_type": "recent", "rpp": 100}))
-		body = json.loads(response.body)
-		result_count = len(body['results'])
-		now = datetime.datetime.utcnow()
-		raw_oldest_tweet_at = body['results'][-1]['created_at']
-		oldest_tweet_at = datetime.datetime.strptime(raw_oldest_tweet_at,
-				"%a, %d %b %Y %H:%M:%S +0000")
-		seconds_diff = time.mktime(now.timetuple()) - \
-				time.mktime(oldest_tweet_at.timetuple())
-		tweets_per_second = float(result_count) / seconds_diff
-		self.write("""
+    def get(self):
+        query = self.get_argument('q')
+        request, response = request_twitter('https://api.twitter.com/1.1/search/tweets.json?' + \
+                                            urllib.urlencode({'q': query, "result_type": "recent", "count": 100 }),
+                                            access_token,
+                                            access_secret)
+        body = json.loads(response)
+        result_count = len(body['statuses'])
+        now = datetime.datetime.utcnow()
+        raw_oldest_tweet_at = body['statuses'][-1]['created_at']
+        oldest_tweet_at = datetime.datetime.strptime(raw_oldest_tweet_at,
+                "%a %b %d %H:%M:%S +0000 %Y")
+        seconds_diff = time.mktime(now.timetuple()) - \
+                time.mktime(oldest_tweet_at.timetuple())
+        tweets_per_second = float(result_count) / seconds_diff
+        self.write("""
 <div style="text-align: center">
 	<div style="font-size: 72px">%s</div>
 	<div style="font-size: 144px">%.02f</div>
@@ -35,8 +56,8 @@ class IndexHandler(tornado.web.RequestHandler):
 </div>""" % (query, tweets_per_second))
 
 if __name__ == "__main__":
-	tornado.options.parse_command_line()
-	app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
-	http_server = tornado.httpserver.HTTPServer(app)
-	http_server.listen(options.port)
-	tornado.ioloop.IOLoop.instance().start()
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
